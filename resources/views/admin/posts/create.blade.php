b29d75e14e283a4cea452d72f115595bfab8e7ab
StefanoCeli <stefanopsn91@gmail.com>
2021-07-05 00:23:52 +0200

    fine Validate

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -3,17 +3,34 @@
 
 <div class="container">
     <h1>Nuovo Post</h1>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     <form action="{{route('admin.posts.store')}}" method="post">
         @csrf
         @method('POST')
     
         <div class="form-group">
             <label for="title">Titolo</label>
-            <input type="text" class="form-control" id="title" name="title">
+            <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title') }}">
+            @error('title')
+            <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>
         <div class="form-group">
             <label for="content">Contenuto</label>
-            <textarea class="form-control" name="content" id="content" rows="6"></textarea>
+            <textarea class="form-control @error('content') is-invalid @enderror" name="content" id="content" rows="6">{{ old('content') }}</textarea>
+            @error('content')
+            <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>
         <div>
             <button type="submit" class="btn btn-primary">Submit</button>
--- resources/views/admin/posts/edit.blade.php
+++ resources/views/admin/posts/edit.blade.php
@@ -3,17 +3,32 @@
 
 <div class="container">
     <h1>Modifica: <a href="{{route('admin.posts.show',$post)}}">{{$post->title}}</a></h1>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
     <form action="{{route('admin.posts.update',$post)}}" method="post">
         @csrf
         @method('PATCH')
     
         <div class="form-group">
             <label for="title">Titolo</label>
-            <input type="text" class="form-control" id="title" name="title" value="{{ $post->title }}">
+            <input type="text" class="form-control  @error('title') is-invalid @enderror" id="title" name="title" value="{{ old('title', $post->title) }}">
+            @error('title')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>
         <div class="form-group">
             <label for="content">Contenuto</label>
-            <textarea class="form-control" name="content" id="content" rows="6">{{ $post->content }}</textarea>
+            <textarea class="form-control  @error('content') is-invalid @enderror" name="content" id="content" rows="6">{{ old('content', $post->content) }}</textarea>
+            @error('content')
+                <p class="text-danger">{{ $message }}</p>
+            @enderror
         </div>
         <div>
             <button type="submit" class="btn btn-primary">Submit</button>
